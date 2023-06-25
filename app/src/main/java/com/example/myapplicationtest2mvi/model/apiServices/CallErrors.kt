916437507c44a905e7de8e26c15e6aa17ad672c9
m.monemi <1234!@#$>
2023-06-25 12:56:22 +0430

    Project has been created! it's base structure is set up and it's packaging is done .

---
+++ app/src/main/java/com/example/myapplicationtest2mvi/model/apiServices/CallErrors.kt
@@ -0,0 +1,9 @@
+package com.example.myapplicationtest2mvi.model.apiServices
+
+sealed class CallErrors {
+
+    object ErrorEmptyData : CallErrors()
+    object ErrorServer : CallErrors()
+    data class ErrorException(val throwable : Throwable) : CallErrors()
+
+}
---
+++ app/src/main/java/com/example/myapplicationtest2mvi/ui/views/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplicationtest2mvi.ui.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplicationtest2mvi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
